a1e5b21e9b19b3ae7aafc45d262a7824274f873f
uta-dcm0927 <64230236+uta-dcm0927@users.noreply.github.com>
2021-09-24 23:03:55 -0500

    Update DetailActivity.java
    censored youtube API key

--- app/src/main/java/com/example/flixster/DetailActivity.java
+++ app/src/main/java/com/example/flixster/DetailActivity.java
@@ -23,7 +23,7 @@ import okhttp3.Headers;
 
 public class DetailActivity extends YouTubeBaseActivity {
 
-    private static final String YOUTUBE_API_KEY = "AIzaSyBb0edfbflmXfkpZFYJLUak13pwSHZUJTw";
+    private static final String YOUTUBE_API_KEY = "place youtube API here";
     public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
     TextView tvTitle;
